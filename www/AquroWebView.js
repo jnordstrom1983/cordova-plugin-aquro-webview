335fe6d92dabedcff8bb546c98fa79dd201e8f4b
Johan Nordstrom <nordiz@gmail.com>
2016-10-18 16:26:52 +0200

    Updated webview component to 0.0.2

--- www/AquroWebView.js
+++ www/AquroWebView.js
@@ -1,5 +1,28 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "AquroWebView", "coolMethod", [arg0]);
+exports.CreateView = function(name, url, top, left, width, height, success, error) {
+    exec(success, error, "AquroWebView", "CreateView", [name,url,top,left,width,height]);
+};
+
+exports.MoveView = function(name, top, left, width, height, success, error) {
+    exec(success, error, "AquroWebView", "MoveView", [name,top,left,width,height]);
+};
+
+exports.ShowView = function(name, success, error) {
+    exec(success, error, "AquroWebView", "ShowView", [name]);
+};
+exports.HideView = function(name, success, error) {
+    exec(success, error, "AquroWebView", "HideView", [name]);
+};
+
+exports.DeleteView = function(name, success, error) {
+    exec(success, error, "AquroWebView", "DeleteView", [name]);
+};
+
+exports.RemoveAllViews = function(success, error) {
+    exec(success, error, "AquroWebView", "RemoveAllViews", []);    
+};
+
+exports.SetURL = function(name,url, success, error) {
+    exec(success, error, "AquroWebView", "SetURL", [name, url]);
 };
